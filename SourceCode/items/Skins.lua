ae8100b6941f3196ec6e67dc326ae5f710ef6a50
SparklesRolf <keldeolv.100@gmail.com>
2025-10-06 07:45:22 -0400

    v1.1.1m Update

---
+++ SourceCode/items/Skins.lua
@@ -0,0 +1,46 @@
+local furry_mod = SMODS.current_mod
+
+SMODS.DeckSkin {
+    key = 'clubs_skin',
+    suit = 'Clubs',
+    ranks = {'Jack', 'Queen', 'King'},
+    lc_atlas = 'clubscollab_lc',
+    hc_atlas = 'clubscollab_hc',
+    pos_style = 'collab'
+}
+
+SMODS.DeckSkin {
+    key = 'diamonds_skin',
+    suit = 'Diamonds',
+    ranks = {'Jack', 'Queen', 'King'},
+    lc_atlas = 'diamondscollab_lc',
+    hc_atlas = 'diamondscollab_hc',
+    pos_style = 'collab'
+}
+
+SMODS.DeckSkin {
+    key = 'hearts_skin',
+    suit = 'Hearts',
+    ranks = {'Jack', 'Queen', 'King'},
+    lc_atlas = 'heartscollab_lc',
+    hc_atlas = 'heartscollab_hc',
+    pos_style = 'collab'
+}
+
+SMODS.DeckSkin {
+    key = 'spades_skin',
+    suit = 'Spades',
+    ranks = {'Jack', 'Queen', 'King'},
+    lc_atlas = 'spadescollab_lc',
+    hc_atlas = 'spadescollab_hc',
+    pos_style = 'collab'
+}
+
+SMODS.DeckSkin {
+    key = 'star_skin',
+    suit = 'fur_stars',
+    ranks = {'Jack', 'Queen', 'King'},
+    lc_atlas = 'starscollab_lc',
+    hc_atlas = 'starscollab_hc',
+    pos_style = 'collab'
+}

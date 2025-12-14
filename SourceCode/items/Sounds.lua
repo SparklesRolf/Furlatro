9c6779074bbe4a8d65f686cfb2bd5725a8a3d81a
SparklesRolf <keldeolv.100@gmail.com>
2025-12-13 20:11:57 -0500

    V1.2.1 Update

---
+++ SourceCode/items/Sounds.lua
@@ -0,0 +1,37 @@
+local furry_mod = SMODS.current_mod
+local config = furry_mod.config
+
+SMODS.Sound {
+	key = "xchip",
+	path = "MultiplicativeChips.wav"
+}
+
+SMODS.Sound {
+	key = "echip",
+	path = "ExponentialChips.wav"
+}
+
+SMODS.Sound {
+	key = "eechip",
+	path = "TetrationalChips.wav"
+}
+
+SMODS.Sound {
+	key = "eeechip",
+	path = "PentationalChips.wav"
+}
+
+SMODS.Sound {
+	key = "emult",
+	path = "ExponentialMult.wav"
+}
+
+SMODS.Sound {
+	key = "eemult",
+	path = "TetrationalMult.wav"
+}
+
+SMODS.Sound {
+	key = "eeemult",
+	path = "PentationalMult.wav"
+}
